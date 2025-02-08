1cc2cb1cb6087b01a3ecd96bb621fe7895c27735
Sahil-Rawat-001 <rawatsahilwork001@gmail.com>
2025-02-08 15:29:55 +0530

    add stacnoclass file implemented without class

---
+++ stacknoclass.cpp
@@ -0,0 +1,84 @@
+// here we implementing stack without class in cpp
+
+/*
+ 
+ stack works on LIFO (Last In First Out) principal
+
+  Push(): This operation insert data inside stack;
+  Pop() : This operation deletes data from stack
+  top() : show or display top element
+  size(): display size of stack
+  peek():
+
+  Overflow: It is a condition when stack is full and someone perform push ops
+  Underflow: It is a condition when stack is empty and someone perform pop ops
+*/
+
+#include<iostream>
+using namespace std;
+
+// n is the size of stack
+#define n 10
+
+
+int stack[n]; // this is stack
+int top = -1; // this is top initially has -1 value which means stack is empty
+
+
+// push operation function
+void push(int x){
+ 
+ // check before pushing is stack has enough space or not
+ if(top == n - 1){
+
+    cout << "Overflow !!! : stack is full" << endl;
+ } else {
+
+    top++; // increase top first
+    stack[top] = x;
+ }
+}
+
+void pop(){
+
+    if(top ==  -1){
+
+        cout << "Uverflow !!! : stack is empty" << endl;
+    } else {
+
+        stack[top];
+        top--; // decrease top
+    }
+}
+
+void peek(){
+     
+    if(top == -1)
+    cout << "Underflow !!! : stack is full" << endl; 
+    else
+    cout << "The current top element is: " << stack[top] << endl;
+}
+
+void display(){
+
+    for(int i = 0; i <= top; i++){
+
+        cout << stack[i] << " ";
+    }
+}
+
+int main(){
+
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+
+    pop();
+
+    peek();
+
+    display();
+
+    return 0;
+}
